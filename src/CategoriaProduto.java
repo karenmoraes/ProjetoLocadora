9c6b60b924f670cd915a25da08386ad5b3b5cf15
ezio_joao <ezio_joao@10.1.48.238>
2013-09-30 09:55:14 -0300

    Adicionar Categoria Produto

---
+++ src/CategoriaProduto.java
@@ -0,0 +1,36 @@
+
+public class CategoriaProduto {
+    
+    private int codigo;
+    private String nome;
+    private String descricao;
+    
+    public CategoriaProduto(){
+        
+    }
+
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+    
+}
