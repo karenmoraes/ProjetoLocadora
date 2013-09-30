d34c635d72f8236e6630a696079f7f26ddaa1f7b
ezio_joao <ezio_joao@10.1.48.238>
2013-09-30 09:52:38 -0300

    Adicionar Pessoa Funcionario

--- src/Funcionario.java
+++ src/Funcionario.java
@@ -1,6 +1,7 @@
 
 public class Funcionario extends Pessoa{
     private double salario;
+    private int codigo;
 
     public double getSalario() {
         return salario;
@@ -9,4 +10,13 @@ public class Funcionario extends Pessoa{
     public void setSalario(double salario) {
         this.salario = salario;
     }
+
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+    
 }
