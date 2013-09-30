4613e76284f6d8ea1502dabd75223c2ac63cbbcb
ezio_joao <ezio_joao@10.1.48.238>
2013-09-30 10:10:30 -0300

    Adicionar Classe Produto

---
+++ src/Produto.java
@@ -0,0 +1,55 @@
+
+
+public class Produto {
+    private int codigo;
+    private String nome;
+    private double valor;
+    private CategoriaProduto categoria;
+    private int quantEstoque;
+
+    public Produto(){
+        
+    }
+    
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public double getValor() {
+        return valor;
+    }
+
+    public void setValor(double valor) {
+        this.valor = valor;
+    }
+
+    public CategoriaProduto getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(CategoriaProduto categoria) {
+        this.categoria = categoria;
+    }
+
+    public int getQuantEstoque() {
+        return quantEstoque;
+    }
+
+    public void setQuantEstoque(int quantEstoque) {
+        this.quantEstoque = quantEstoque;
+    }
+    
+    
+}
